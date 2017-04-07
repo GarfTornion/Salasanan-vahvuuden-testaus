a978785ddd9212905a36369c7732840f6fc7f7a6
GarfTornion <billy.ward@hotmail.com>
2017-04-07 20:49:52 +0300

    Javadokkia, toimintoja, nimiä
    Lisää monille metodeille ja luokille Javadoc-tietoa. Uusia toimintoja ja
    ominaisuuksia: historian pystyy tyhjentämään ja historiaan kirjatut
    tiedot sisältävät päivämäärän ja ajan. Joitakin metodi-nimiä muutettu
    sopivammiksi.

--- salasana ohjelma.java
+++ salasana ohjelma.java
@@ -1,9 +1,17 @@
 import java.util.Scanner;
 import java.io.*;
-
+import java.util.Date;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+/**
+ *   <code>salasanan_vahvuus</code> luokka esittää salasanoja testaavan ohjelman toiminnan.
+ * @author Billy Ward
+ * @author Emil Oksanen
+ */
 public class salasanan_vahvuus {
 
 	private static final Scanner user_input = new Scanner( System.in );
+	private static final DateFormat aikatyyli = new SimpleDateFormat("yyyy/MM/dd HH:mm:ss");
 	
 	public static void main(String[] args) throws FileNotFoundException, IOException {
 		boolean lopetus = false;
@@ -11,8 +19,10 @@ public class salasanan_vahvuus {
 		System.out.println("Pääset alkuun seuraavilla komennoilla:");
 		System.out.println("testaa	 = käynnistä salasanan testaus");
 		System.out.println("historia = näytä testattujen salasanojen historia ja tulokset");
+		System.out.println("tyhjennä = tyhjennä historia");
 		System.out.println("lopeta	 = sulje ohjelma");
 		do {
+			System.out.print(">");
 			String komento = user_input.next();
 			if (komento.equals("testaa")) {
 				final Scanner lukija = new Scanner(new File("kriteerit.txt"));
@@ -24,23 +34,45 @@ public class salasanan_vahvuus {
 				}//tiedoston lukeminen
 				lukija.close();
 				
+				System.out.print(">");
 				String salasana = user_input.next();
 				int tulokset[] = tarkista_kriteerit(salasana);
 				System.out.println("Salasanasi täytti " + tulokset[0] + "/" + tulokset[1] + " kriteereistä.");
-				vahvuus(tulokset);
+				tarkista_vahvuus(tulokset);
+				
+				Date päivämäärä = new Date();
 				
 				PrintWriter kirjoittaja = new PrintWriter(new FileWriter("historia.txt", true));
-				kirjoittaja.println(salasana + " " + tulokset[0] + "/" + tulokset[1]);
+				kirjoittaja.println(aikatyyli.format(päivämäärä) + " " + salasana + " " + tulokset[0] + "/" + tulokset[1]);
 				kirjoittaja.close();
 			}//if
+			
 			else if (komento.equals("historia")) {
-				final Scanner tiedosto_lukija = new Scanner(new File("historia.txt"));
+				try {
+					final Scanner tiedosto_lukija = new Scanner(new File("historia.txt"));
 				while (tiedosto_lukija.hasNext()) {
 					String sisältö = tiedosto_lukija.nextLine();
 					System.out.println(sisältö);
 				}//while
 				tiedosto_lukija.close();
+				}//try
+				catch (FileNotFoundException f) {
+					System.out.println("Hupsista! Tapahtui " + f + "-virhe.");
+					System.out.println("Kokeile ensin testata ohjelmalla jotain salasanaa, ja sitten kokeile komentoa uudestaan.");
+				}
 			}//else if
+			else if (komento.equals("tyhjennä")) {
+				try {
+					System.out.println("Historia tyhjennetään...");
+					PrintWriter kirjoittaja = new PrintWriter(new FileWriter("historia.txt"));
+					kirjoittaja.flush();
+					kirjoittaja.close();
+					System.out.println("Historia on tyhjennetty!");
+				}//try
+				catch (FileNotFoundException f) {
+					System.out.println("Ei ole tyhjennettävää historiaa!");
+				}
+			}
 			else if (komento.equals("lopeta")) {
 				lopetus = true;
 			}//else if
@@ -50,8 +82,19 @@ public class salasanan_vahvuus {
 		
 	}//main
 
+	/**
+	 * <code>Tarkista_kriteerit</code> metodi tarkistaa täyttääkö annettu syöte
+	 * kriteerit. Metodi kutsuu sisällää alempia metodeja jotka palauttavat
+	 * boolean-tyyppisiä arvoja.
+	 * 
+	 * @param salasana
+	 *            Käyttäjän antama salasana-syöte.
+	 * @return Palauttaa taulukon, joka sisältää kaksi arvoa. Ensimmäinen arvo
+	 *         on kriteerit täyttäneet ominaisuudet, ja toinen arvo on
+	 *         testattujen kriteerien lukumäärä.
+	 */
 	public static int[] tarkista_kriteerit(String salasana) {
-		boolean kriteerit[] = {pituus(salasana), tarkista_numero(salasana), tarkista_kirjainkoko(salasana),
+		boolean kriteerit[] = {tarkista_pituus(salasana), tarkista_numero(salasana), tarkista_kirjainkoko(salasana),
 		tarkista_erikoismerkki(salasana), tarkista_hupsu(salasana), tarkista_vastaus(salasana),
 		tarkista_gaben(salasana), tarkista_hymio(salasana)};
 		int läpäissyt = 0;
@@ -70,7 +113,15 @@ public class salasanan_vahvuus {
 		return new int[] {läpäissyt, totaali};
 	}//kriteerit
 		
-	public static boolean pituus(String salasana) {
+	/**
+	 * <code>Tarkista_pituus</code>-metodi tarkistaa onko syötteen pituus
+	 * annettujen ohjeiden mukainen.
+	 * 
+	 * @param salasana
+	 *            Käyttäjän antama salasana-syöte.
+	 * @return Palauttaa totuus-arvon.
+	 */
+	public static boolean tarkista_pituus(String salasana) {
 		if (salasana.length() >= 10) {
 			return true;
 		}
@@ -79,15 +130,33 @@ public class salasanan_vahvuus {
 		}
 	}//pituus
 	
-	public static void vahvuus(int[] tulokset) {
+	/**
+	 * <code>Tarkista_vahvuus</code>-metodi antaa käyttäjälle viimeisen
+	 * palautteen salasanasta.
+	 * 
+	 * @param tulokset
+	 *            Täytettyjen kriteerien ja kaikkien kriteerien lukumäärä.
+	 *            Saadaan <code>tarkista_kriteerit</code>-metodista.
+	 */
+	public static void tarkista_vahvuus(int[] tulokset) {
 		if (tulokset[0] == tulokset[1]) {
 			System.out.println("Onneksi olkoon! Salasanasi täyttää kaikki kriteerit!");
 		}
-		else {
-			System.out.println("Valitettavasti salasanasi ei täytä kaikki kriteerejä, kokeile uudelleen!");
+		else if (tulokset[0] == 0) {
+			System.out.println("Salasanasi ei täyttänyt yhtään kriteeriä! ÄLÄ MISSÄÄN NIMESSÄ KÄYTÄ TÄTÄ SALASANAA!");
 		}
+		else
+			System.out.println("Salasanasi ei täyttäny kaikkia kriteerejä!");
 	}//vahvuus
 	
+	/**
+	 * <code>Tarkista_numero</code>-metodi tarkistaa onko annetussa syötteessä
+	 * numeroa.
+	 * 
+	 * @param salasana
+	 *            Käyttäjän antama salasana-syöte.
+	 * @return Palauttaa totuus-arvon.
+	 */
 	public static boolean tarkista_numero(String salasana) {
 		int i = 0;
 		int numero = 0;
@@ -104,6 +173,14 @@ public class salasanan_vahvuus {
 			return false;
 	}//tarkista_numero
 	
+	/**
+	 * <code>Tarkista_erikoismerkki</code>-metodi tarkistaa onko annetussa
+	 * syötteessä erikoismerkkiä.
+	 * 
+	 * @param salasana
+	 *            Käyttäjän antama salasana-syöte.
+	 * @return Palauttaa totuus-arvon.
+	 */
 	public static boolean tarkista_erikoismerkki(String salasana) {
 		int i = 0;
 		int erikoismerkki = 0;
@@ -119,7 +196,14 @@ public class salasanan_vahvuus {
 			return false;
 	}//tarkista_erikoismerkki
 		
-
+	/**
+	 * <code>Tarkista_kirjainkoko</code>-metodi tarkistaa löytyykö annetusta
+	 * syötteestä sekä isoja, että pienie kirjaimia.
+	 * 
+	 * @param salasana
+	 *            Käyttäjän antama salasana-syöte.
+	 * @return Palauttaa totuus-arvon.
+	 */
 	public static boolean tarkista_kirjainkoko(String salasana) {
 		int i = 0;
 		int numero1 = 0;
@@ -141,6 +225,14 @@ public class salasanan_vahvuus {
 			return false;
 	}//tarkista_kirjainkoko
 	
+	/**
+	 * <code>Tarkista_hupsu</code>-metodi tarkistaa onko syötteessä merkkijonoa
+	 * "hupsu".
+	 * 
+	 * @param salasana
+	 *            Käyttäjän antama salasana-syöte.
+	 * @return Palauttaa totuus-arvon.
+	 */
 	public static boolean tarkista_hupsu(String salasana) {
 		if (salasana.indexOf("hupsu") != -1)
 			return true;
@@ -148,6 +240,14 @@ public class salasanan_vahvuus {
 			return false;
 	}//tarkista_hupsu
 	
+	/**
+	 * <code>Tarkista_vastaus</code>-metodi tarkistaa onko syötteessä vastausta
+	 * kysyttyyn yhtälöön.
+	 * 
+	 * @param salasana
+	 *            Käyttäjän antama salasana-syöte.
+	 * @return Palauttaa totuus-arvon.
+	 */
 	public static boolean tarkista_vastaus(String salasana) {
 		if (salasana.indexOf("11") != -1)
 			return true;
@@ -155,6 +255,14 @@ public class salasanan_vahvuus {
 			return false;
 	}//tarkista_vastaus
 	
+	/**
+	 * <code>Tarkista_gaben</code>-metodi tarkistaa onko syötteessä merkkijonoa
+	 * "gaben".
+	 * 
+	 * @param salasana
+	 *            Käyttäjän antama salasana-syöte.
+	 * @return Palauttaa totuus-arvon.
+	 */
 	public static boolean tarkista_gaben(String salasana) {
 		if (salasana.indexOf("gaben") != -1)
 			return true;
@@ -162,6 +270,14 @@ public class salasanan_vahvuus {
 			return false;
 	}//tarkista_gaben
 	
+	/**
+	 * <code>Tarkista_hymio</code>-metodi tarkistaa löytyykö syötteestä
+	 * :D-hymiötä.
+	 * 
+	 * @param salasana
+	 *            Käyttäjän antama salasana-syöte.
+	 * @return Palauttaa totuus-arvon.
+	 */
 	public static boolean tarkista_hymio(String salasana) {
 		if (salasana.indexOf(":D") != -1)
 			return true;
